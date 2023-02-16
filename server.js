ebdbdf15316c5e775bbdf3f337975dfc01618247
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-02-17 00:46:07 +0530

    HOST DEPLOYMENT

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = 'const hostname = '0.0.0.0';
+';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
