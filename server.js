57b4f6d20baadda57d847d4bd645041f110f5d37
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-02-17 00:54:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = 'https://testapi-x3is.onrender.com/';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
