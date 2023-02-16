cd395cd8c8093d7e7a71732cd07268f43277cb50
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-02-17 00:43:15 +0530

    TEST#1

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 200;
+  res.end(JSON.stringify( '0d657f444BF2AA726a085067C4E26e782d837452' ));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
