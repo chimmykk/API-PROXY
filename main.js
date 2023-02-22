ae49af3737c3a843be7d278e93a05e2d6bbcf991
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-02-22 08:20:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,20 +21,46 @@ const nftContract = new web3.eth.Contract(contractABI, contractAddress);
 const secp256k1 = new ec('secp256k1');
 
 const server = http.createServer(app);
-
 app.get('/verify-proxy-ownership', async (req, res) => {
   const userWalletAddress = req.query.userWalletAddress;
   const nftTokenId = req.query.nftTokenId;
-  //again here -> to go with specific TokenID
-  // app.get('/verify-proxy-ownership', async (req, res) =>
- 
-
+  
   // Check if user's wallet holds at least one NFT
   const hasNFT = await nftContract.methods.balanceOf(userWalletAddress).call();
   if (hasNFT <= 0) {
     res.status(401).send('User does not hold any NFTs');
     return;
-  } //
+  } 
+
+  // Verify ownership using the user's private key and the NFT's token ID
+  const privateKey = 'xxxx'; // Replace with user's private key
+  const msgHash = web3.utils.keccak256(`You are verifying ownership of NFT with ID ${nftTokenId}`);
+  const msgHashHex = `0x${msgHash}`;
+  const msgHashBytes = Buffer.from(msgHashHex.slice(2), 'hex');
+  const signature = secp256k1.keyFromPrivate(privateKey).sign(msgHashBytes);
+  const r = signature.r.toString(16);
+  const s = signature.s.toString(16);
+  const v = signature.recoveryParam + 27;
+  const signedMessage = {
+    messageHash: msgHashHex,
+    v: `0x${v.toString(16)}`,
+    r: `0x${r.padStart(64, '0')}`,
+    s: `0x${s.padStart(64, '0')}`,
+  };
+  const address = await web3.eth.personal.ecRecover(msgHashHex, signedMessage);
+  if (address.toLowerCase() !== userWalletAddress.toLowerCase()) {
+    res.status(401).send('Invalid signature');
+    return;
+  }
+
+  res.status(200).json({ message: 'Ownership verified successfully' });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+This code will check if a user owns an NFT from the contract address  assume if yes then provide proxy access.
+
 
   // Verify ownership using the user's private key and the NFT's token ID
   const privateKey = 'xxxx'; // Replace with user's private key
