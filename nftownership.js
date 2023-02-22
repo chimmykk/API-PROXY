95928f6bb96c2c3ebfca18968983d48483778f73
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-02-22 17:36:01 +0530

    Update nftownership.js
    REVIEW NEEDED

--- nftownership.js
+++ nftownership.js
@@ -1,10 +1,3 @@
-//either perform using upgrade-contract
-
-//or use and API-end point to verify ownership of the NFT using token ID
-
-//IF fetch-api succesfully - provide
-//proxy-ownership
-
 const http = require('http');
 const express = require('express');
 const { ec } = require('elliptic');
@@ -13,66 +6,70 @@ const contractABI = require('./NFTContractABI.json');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-//infura
+
+const app = express();
+app.use(express.json());
+
+// Initialize Web3 with your Ethereum network's endpoint URL
 const web3 = new Web3('https://goerli.infura.io/v3/3376a33c419a4d249d680fa54ff8b6bf');
 
 // Initialize the contract instance using its ABI and address
-const contractAddress = '0xF4ee95274741437636e748DdAc70818B4ED7d043'; // Replace with your contract's address
+const contractAddress = '0xF4ee95274741437636e748DdAc70818B4ED7d043'; 
 const nftContract = new web3.eth.Contract(contractABI, contractAddress);
-
-// Initialize the elliptic curve cryptography library for signing and verifying messages
-//for the user's wallet -> proxy wallet 
 const secp256k1 = new ec('secp256k1');
 
-const app = express();
-
-app.use(express.json());
-
-// Endpoint to verify ownership of an NFT and grant proxy access
-app.post('/verify', async (req, res) => {
-  try {
-    const userWalletAddress = req.body.address;
-    const signature = req.body.signature;
-    // the signature here is being /verify using the api-endpoint
-
-    // Check if the NFT is still owned by the user
-    const tokenId = 1; // Replace with the ID of your NFT
-    // here tokenId= is specified we can change to balanceOf 
-    // allow any users holding NFTs to verify the process
-    const currentOwner = await nftContract.methods.ownerOf(tokenId).call();
-    if (currentOwner !== userWalletAddress) {
-      return res.status(400).json({ error: 'The NFT is not owned by the user' });
-    }
-
-    // Verify the signature with the user's wallet address
-    const messageToVerify = 'Access Request';
-    const messageHash = web3.utils.sha3(messageToVerify);
-    const signatureBytes = web3.utils.hexToBytes(signature);
-    const signatureObject = {
-      r: signatureBytes.slice(0, 32),
-      s: signatureBytes.slice(32, 64),
-      recoveryParam: Number('0x' + signatureBytes.slice(64, 66)) - 27
-    };
-    const publicKeyObject = secp256k1.recoverPubKey(messageHash, signatureObject, signatureObject.recoveryParam, true);
-    const publicKey = publicKeyObject.encode('hex');
-    if (publicKey.toLowerCase() !== userWalletAddress.toLowerCase()) {
-      return res.status(401).json({ error: 'Invalid signature' });
-    }
-
-    // Grant proxy ownership of the NFT
-    const proxyAddress = '0x94ee95274741437636e748DdAc70818B4ED7d043'; 
-    //Replace Proxy address.
-    await nftContract.methods.approve(proxyAddress, tokenId).send({ from: userWalletAddress });
-
-    return res.status(200).json({ success: true });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Something went wrong' });
-  }
-});
-//localhost deployment
 const server = http.createServer(app);
-//Install npm -i express,web3,http
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+// Verify ownership of an NFT by a user wallet
+app.get('/verify-ownership', async (req, res) => {
+  const userWalletAddress = req.query.userWalletAddress;
+  const nftTokenId = req.query.nftTokenId;
+
+          // Check if user's wallet holds at least one NFT
+  const hasNFT = await nftContract.methods.balanceOf(userWalletAddress).call();
+  if (hasNFT <= 0) {
+    res.status(401).send('User does not hold any NFTs');
+    return;
+  }
+  res.json(signedMessage);
+return;
+
+  // Get the owner of the NFT
+  const owner = await nftContract.methods.ownerOf(nftTokenId).call();
+  if (owner.toLowerCase() !== userWalletAddress.toLowerCase()) {
+    res.status(401).send('User does not own the specified NFT');
+    return;
+  }
+
+  res.status(200).json({ message: 'Ownership verified successfully' });
 });
+
+// Grant access or sign a gated page using a proxy wallet
+app.get('/grant-access', async (req, res) => {
+  const proxyWalletAddress = req.query.proxyWalletAddress;
+  const gatedPageId = req.query.gatedPageId;
+
+  // Check if proxy wallet is authorized to grant access
+  const isAuthorized = await nftContract.methods.isAuthorized(proxyWalletAddress).call();
+  if (!isAuthorized) {
+    res.status(401).send('Proxy wallet is not authorized to grant access');
+    return;
+  }
+
+  // Grant access or sign gated page using the proxy wallet's private key
+  const privateKey = 'xxxx'; // Replace with proxy wallet's private key
+  const msgHash = web3.utils.keccak256(`You are granting access to gated page with ID ${gatedPageId}`);
+  const msgHashHex = `0x${msgHash}`;
+  const msgHashBytes = Buffer.from(msgHashHex.slice(2), 'hex');
+  const signature = secp256k1.keyFromPrivate(privateKey).sign(msgHashBytes);
+  const r = signature.r.toString(16);
+  const s = signature.s.toString(16);
+  const v = signature.recoveryParam + 27;
+  const signedMessage = {
+    messageHash: msgHashHex,
+    v: `0x${v.toString(16)}`,
+    r: `0x${r.padStart(64, '0')}`,
+    s: `0x${s.padStart(64, '0')}`,
+  };
+
+  res
